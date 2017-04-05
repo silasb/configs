4caf5bfe0eb6509e94263a119fb05bb5632cb579
silasb <silas.baronda@gmail.com>
2017-04-05 12:16:26 -0400

    Cleaning up hammerspoon configuration

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -16,6 +16,102 @@ local yankkey = {"⌥", "⌃","⇧"}
 local pushkey = {"⌃", "⌘"}
 local shiftpushkey= {"⌃", "⌘", "⇧"}
 
+-- -----------------
+-- Window management
+-- -----------------
+
+-- Movement hotkeys
+hs.hotkey.bind(nudgekey, 'down', function() nudge(0,100) end) 	--down
+hs.hotkey.bind(nudgekey, "up", function() nudge(0,-100) end)	--up
+hs.hotkey.bind(nudgekey, "right", function() nudge(100,0) end)	--right
+hs.hotkey.bind(nudgekey, "left", function() nudge(-100,0) end)	--left
+
+-- Resize hotkeys
+hs.hotkey.bind(yankkey, "up", function() yank(0,-100) end) -- yank bottom up
+hs.hotkey.bind(yankkey, "down", function() yank(0,100) end) -- yank bottom down
+hs.hotkey.bind(yankkey, "right", function() yank(100,0) end) -- yank right side right
+hs.hotkey.bind(yankkey, "left", function() yank(-100,0) end) -- yank right side left
+
+-- Push to screen edge
+hs.hotkey.bind(pushkey,"left", function() push(0,0,0.5,1) end) 		-- left side
+hs.hotkey.bind(pushkey,"right", function() push(0.5,0,0.5,1) end)	-- right side
+hs.hotkey.bind(pushkey,"up", function()	push(0,0,1,0.5) end) 		-- top half
+hs.hotkey.bind(pushkey,"down", function()	push(0,0.5,1,0.5) end)	-- bottom half
+
+-- Center window with some room to see the desktop
+hs.hotkey.bind(pushkey, "m", function() push(0.05,0.05,0.9,0.9) end)
+
+-- Fullscreen
+hs.hotkey.bind(pushkey, "f", function() push(0,0,1,1) end)
+
+-- Chat windows (arrange in grid of 5 on right hand of screen)
+--hs.hotkey.bind(hyper, "1", function() push(0.8,0,0.2,0.2) end)
+--hs.hotkey.bind(hyper, "2", function() push(0.8,0.2,0.2,0.2) end)
+--hs.hotkey.bind(hyper, "3", function() push(0.8,0.4,0.2,0.2) end)
+--hs.hotkey.bind(hyper, "4", function() push(0.8,0.6,0.2,0.2) end)
+--hs.hotkey.bind(hyper, "5", function() push(0.8,0.8,0.2,0.2) end)
+
+-- Move a window between monitors
+hs.hotkey.bind(pushkey,"1", function() moveToMonitor(1) end) -- Move to first monitor
+hs.hotkey.bind(shiftpushkey,"1", function() -- Move to first monitor and fullscreen
+	moveToMonitor(1)
+	push(0,0,1,1)
+end)
+
+hs.hotkey.bind(pushkey,"2", function() moveToMonitor(2) end) -- Move to second monitor
+hs.hotkey.bind(shiftpushkey,"2", function() -- Move to second monitor and fullscreen
+	moveToMonitor(2)
+	push(0,0,1,1)
+end)
+
+hs.hotkey.bind(pushkey, "w", function() mouseHighlight() end)
+
+--config reloading. manual (from getting started guide):
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
+  hs.alert.show("Config loaded")
+end)
+
+-- Mouse circle example from getting started guide.
+-- This places a red circle around the mouse pointer (because I lose it a lot)
+local mouseCircle = nil
+local mouseCircleTimer = nil
+
+function mouseHighlight()
+    -- Delete an existing highlight if it exists
+    if mouseCircle then
+        mouseCircle:delete()
+        if mouseCircleTimer then
+            mouseCircleTimer:stop()
+        end
+    end
+    -- Get the current co-ordinates of the mouse pointer
+    mousepoint = hs.mouse.get()
+    -- Prepare a big red circle around the mouse pointer
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+    mouseCircle:setFill(false)
+    mouseCircle:setStrokeWidth(5)
+    mouseCircle:show()
+
+    -- Set a timer to delete the circle after 3 seconds
+    mouseCircleTimer = hs.timer.doAfter(2, function() mouseCircle:delete() end)
+end
+
+
+--and magic (from getting started guide):
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+
 -- --------------------------------------------------------
 -- Helper functions - these do all the heavy lifting below.
 -- Names are roughly stolen from same functions in Slate :)
@@ -68,98 +164,5 @@ function moveToMonitor(x)
 	win:moveToScreen(newScreen)
 end
 
--- -----------------
--- Window management
--- -----------------
-
--- Movement hotkeys
-hs.hotkey.bind(nudgekey, 'down', function() nudge(0,100) end) 	--down
-hs.hotkey.bind(nudgekey, "up", function() nudge(0,-100) end)	--up
-hs.hotkey.bind(nudgekey, "right", function() nudge(100,0) end)	--right
-hs.hotkey.bind(nudgekey, "left", function() nudge(-100,0) end)	--left
-
--- Resize hotkeys
-hs.hotkey.bind(yankkey, "up", function() yank(0,-100) end) -- yank bottom up
-hs.hotkey.bind(yankkey, "down", function() yank(0,100) end) -- yank bottom down
-hs.hotkey.bind(yankkey, "right", function() yank(100,0) end) -- yank right side right
-hs.hotkey.bind(yankkey, "left", function() yank(-100,0) end) -- yank right side left
-
--- Push to screen edge
-hs.hotkey.bind(pushkey,"left", function() push(0,0,0.5,1) end) 		-- left side
-hs.hotkey.bind(pushkey,"right", function() push(0.5,0,0.5,1) end)	-- right side
-hs.hotkey.bind(pushkey,"up", function()	push(0,0,1,0.5) end) 		-- top half
-hs.hotkey.bind(pushkey,"down", function()	push(0,0.5,1,0.5) end)	-- bottom half
-
--- Center window with some room to see the desktop
-hs.hotkey.bind(pushkey, "m", function() push(0.05,0.05,0.9,0.9) end)
-
--- Fullscreen
-hs.hotkey.bind(pushkey, "f", function() push(0,0,1,1) end)
-
--- Chat windows (arrange in grid of 5 on right hand of screen)
---hs.hotkey.bind(hyper, "1", function() push(0.8,0,0.2,0.2) end)
---hs.hotkey.bind(hyper, "2", function() push(0.8,0.2,0.2,0.2) end)
---hs.hotkey.bind(hyper, "3", function() push(0.8,0.4,0.2,0.2) end)
---hs.hotkey.bind(hyper, "4", function() push(0.8,0.6,0.2,0.2) end)
---hs.hotkey.bind(hyper, "5", function() push(0.8,0.8,0.2,0.2) end)
-
--- Move a window between monitors
-hs.hotkey.bind(pushkey,"1", function() moveToMonitor(1) end) -- Move to first monitor
-hs.hotkey.bind(shiftpushkey,"1", function() 											 -- Move to first monitor and fullscreen
-	moveToMonitor(1)
-	push(0,0,1,1)
-end)
-
-hs.hotkey.bind(pushkey,"2", function() moveToMonitor(2) end) -- Move to second monitor
-hs.hotkey.bind(shiftpushkey,"2", function() 											 -- Move to second monitor and fullscreen
-	moveToMonitor(2)
-	push(0,0,1,1)
-end)
-
--- Mouse circle example from getting started guide.
--- This places a red circle around the mouse pointer (because I lose it a lot)
-local mouseCircle = nil
-local mouseCircleTimer = nil
-
-function mouseHighlight()
-    -- Delete an existing highlight if it exists
-    if mouseCircle then
-        mouseCircle:delete()
-        if mouseCircleTimer then
-            mouseCircleTimer:stop()
-        end
-    end
-    -- Get the current co-ordinates of the mouse pointer
-    mousepoint = hs.mouse.get()
-    -- Prepare a big red circle around the mouse pointer
-    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
-    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
-    mouseCircle:setFill(false)
-    mouseCircle:setStrokeWidth(5)
-    mouseCircle:show()
-
-    -- Set a timer to delete the circle after 3 seconds
-    mouseCircleTimer = hs.timer.doAfter(2, function() mouseCircle:delete() end)
-end
-hs.hotkey.bind(pushkey, "w", mouseHighlight)
-
---config reloading. manual (from getting started guide):
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
-  hs.reload()
-  hs.alert.show("Config loaded")
-end)
-
---and magic (from getting started guide):
-function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
